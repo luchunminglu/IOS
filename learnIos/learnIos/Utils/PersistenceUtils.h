66c2e2e047d51e31343760db93c9398dd81df9cf
luchunminglu <luchunminglu@gmail.com>
2016-02-17 00:11:49 +0800

    cc

--- learnIos/learnIos/Utils/PersistenceUtils.h
+++ learnIos/learnIos/Utils/PersistenceUtils.h
@@ -7,8 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
 
 @interface PersistenceUtils : NSObject
+
+#pragma mark -File&Directory
 
 /**
  *  获取本程序的/Documents目录
@@ -26,5 +30,79 @@
  *  @return <#return value description#>
  */
 +(NSString*) getTemporaryFile:(NSString*)fileName;
+
+/**
+ *  文件或目录是否存在
+ *
+ *  @param filePath <#filePath description#>
+ *
+ *  @return <#return value description#>
+ */
++(BOOL) fileExist:(NSString*) filePath;
+
+
+#pragma mark -Property list
+
+
+/**
+ *  write array to file,如果文件存在，内容被覆盖,文件编码utf8
+ *
+ *  @param array    <#array description#>
+ *  @param fileName 在ios中应该是/Documents目录或子目录下的文件
+ */
++(void) writeArray:(NSArray*) array toFile:(NSString*) fileName;
+
+/**
+ *  read file
+ *
+ *  @param fileName <#fileName description#>
+ *
+ *  @return <#return value description#>
+ */
++(NSArray*) readArrayFromFile:(NSString*) fileName;
+
+
+/**
+ *  将Dictionary写到文件中，如果文件存在，内容被覆盖,文件编码utf8
+ *
+ *  @param dictionary <#dictionary description#>
+ *  @param fileName   f在ios中应该是/Documents目录或子目录下的文件
+ */
++(void) writeDictionary:(NSDictionary*) dictionary toFile:(NSString*)  fileName;
+
+/**
+ *  read file
+ *
+ *  @param fileName <#fileName description#>
+ *
+ *  @return <#return value description#>
+ */
++(NSDictionary*) readDictionaryFromFile:(NSString*) fileName;
+
+
+#pragma mark -sqlite3
+
+
+
++(sqlite3*) openSqlite:(NSString*) filePath;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
--- learnIos/learnIos/Utils/PersistenceUtils.m
+++ learnIos/learnIos/Utils/PersistenceUtils.m
@@ -8,11 +8,14 @@
 
 #import "PersistenceUtils.h"
 
+
 /**
  *  every application gets its own /Documents folder, and applications are allowed to read and write from only their own /Documents directory.
  */
 @implementation PersistenceUtils
 
+
+#pragma mark -File&Directory
 
 /**
  *  获取本程序的/Documents目录
@@ -37,5 +40,94 @@
     NSString* tempPath = NSTemporaryDirectory();
     return [tempPath stringByAppendingPathComponent:@"tempFile.txt"];
 }
+
+/**
+ *  文件或目录是否存在
+ *
+ *  @param filePath <#filePath description#>
+ *
+ *  @return <#return value description#>
+ */
++(BOOL) fileExist:(NSString*) filePath{
+    NSFileManager* fileManager = [NSFileManager defaultManager];
+    return [fileManager fileExistsAtPath:filePath];
+}
+
+
+#pragma mark -Property list
+
+
+/**
+ *  write array to file，如果文件存在内容被覆盖。,文件编码utf8
+ *
+ *  @param array    <#array description#>
+ *  @param fileName 在ios中应该是/Documents目录或子目录下的文件
+ */
++(void) writeArray:(NSArray*) array toFile:(NSString*) fileName{
+    //If YES, the array is written to an auxiliary file, and then the auxiliary file is renamed to path.
+    [array writeToFile:fileName atomically:YES];
+}
+
+/**
+ *  read file
+ *
+ *  @param fileName <#fileName description#>
+ *
+ *  @return <#return value description#>
+ */
++(NSArray*) readArrayFromFile:(NSString*) fileName{
+    return [NSArray arrayWithContentsOfFile:fileName];
+}
+
+/**
+ *  将Dictionary写到文件中，如果文件存在，内容被覆盖,文件编码utf8
+ *
+ *  @param dictionary <#dictionary description#>
+ *  @param fileName   f在ios中应该是/Documents目录或子目录下的文件
+ */
++(void) writeDictionary:(NSDictionary*) dictionary toFile:(NSString*)  fileName{
+    [dictionary writeToFile:fileName atomically:YES];
+}
+
+/**
+ *  read file
+ *
+ *  @param fileName <#fileName description#>
+ *
+ *  @return <#return value description#>
+ */
++(NSDictionary*) readDictionaryFromFile:(NSString*) fileName{
+    return [NSDictionary dictionaryWithContentsOfFile:fileName];
+}
+
+
+#pragma mark -sqlite3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
