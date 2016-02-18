09674762ffa5ff4467cb88b44be12e8cc0b367cf
luchunminglu <luchunminglu@gmail.com>
2016-02-18 19:13:23 +0800

    cc

---
+++ learnIos/learnIos/Utils/DateUtils.m
@@ -0,0 +1,41 @@
+//
+//  DateUtils.m
+//  learnIos
+//
+//  Created by chunminglu on 16/2/18.
+//  Copyright © 2016年 chunminglu. All rights reserved.
+//
+
+#import "DateUtils.h"
+
+@implementation DateUtils
+
+/**
+ *  将时间转换为字符串
+ *
+ *  @param date   <#date description#>
+ *  @param foramt 如"yyyy-MM-dd"  "yyyy-MM-dd HH:mm:ss"
+ *
+ *  @return <#return value description#>
+ */
++(NSString*) convertToString:(NSDate*) date format:(NSString*) foramt{
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:foramt];
+    return [formatter stringFromDate:date];
+}
+
+/**
+ *  将时间转换为字符串
+ *
+ *  @param str   <#date description#>
+ *  @param foramt 如"yyyy-MM-dd"  "yyyy-MM-dd HH:mm:ss"
+ *
+ *  @return <#return value description#>
+ */
++(NSDate*) convertToDate:(NSString*) str format:(NSString*) foramt{
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:foramt];
+    return [formatter dateFromString:str];
+}
+
+@end
